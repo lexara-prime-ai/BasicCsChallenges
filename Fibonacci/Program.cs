6be08c7dbc87fc0bd88de8c25fa6246c596db365
irfanghat <irfanghat@gmail.com>
2023-07-13 02:55:09 +0300

    Challenge 1: Fibonacci Sequence

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,20 +1,14 @@
-﻿using System;
-
-namespace FIBONACCI
+﻿namespace FIBONACCI
 {
     class SEQUENCE
     {
-        /* INITIAL VALUES  */
+        /* INITIAL VALUES */
         static int p1 = 0, p2 = 1, p3;
         static void Main(string[] args)
         {
-            Console.WriteLine("Please provide an input value: (e.g 10) ");
-            // READ INPUT VALUE AND CONVERT IT TO AN INTEGER
+            Console.WriteLine("Please provide an input value: ");
             int count = Int32.Parse(Console.ReadLine());
-            // PRINT THE INITIAL VALUES IN THE SEQUENCE i.e 0, 1, 1
             Console.Write($"{p1} {p2}");
-
-            // ITERATE UNTIL THE VALUE OF count IS NO LONGER GREATER THAN i
             for (var i = 2; i < count; i++)
             {
                 p3 = p1 + p2;
